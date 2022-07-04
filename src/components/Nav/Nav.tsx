24702746f1477b38d3ac62180c5c34a4ddf2fe95
alan-jouhar <alan.jouhar@gmail.com>
2022-07-04 17:17:39 +0200

    Add Nav item with basic animation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, { CSSProperties } from "react";
+import { Link } from "react-router-dom";
+import "./nav.css";
 type NavProps = {
 	pages: NavItem[];
 };
@@ -8,12 +9,38 @@ type NavItem = {
 	name: string;
 };
 const Nav: React.FC<NavProps> = (props: NavProps) => {
+	const animateItem: React.MouseEventHandler<HTMLLIElement> = (e) => {
+		let traget = e.currentTarget as HTMLLIElement;
+		traget.animate(keyFrameObjects,{
+			duration: 500,
+			iterations:1
+		})
+		traget.classList.toggle("Nav-Item_magnified");
+	};
+
+	const keyFrameObjects: Keyframe[] = [
+		{
+			transform: "scale(0.5)",
+		},
+		{
+			transform: "scale(1)",
+		},
+		{
+			transform: "scale(0.8)",
+		},
+		{
+			transform: "scale(1)",
+		},
+	];
+
 	return (
-		<nav>
-			<ul>
+		<nav className="Nav">
+			<ul className="Nav-List">
 				{props.pages.map((page) => (
-					<li key={page.name}>
-						<Link to={page.path}>{page.name}</Link>
+					<li key={page.name} className="Nav-Item" onClick={animateItem}>
+						<Link to={page.path} className="Nav-Link">
+							{page.name.toUpperCase()}
+						</Link>
 					</li>
 				))}
 			</ul>
