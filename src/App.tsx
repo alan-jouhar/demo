ff1ba378a8a2f929e0ea0423b6c2f8f163136f4e
alan-jouhar <alan.jouhar@gmail.com>
2022-07-05 19:02:01 +0200

    add a new page for contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
-import Main from "./pages/Main"
-import About from "./pages/About"
+import Main from "./pages/Main";
+import About from "./pages/About";
+import Contact from './pages/Contact'
 const App: React.FC = (props) => {
 	return (
 		<BrowserRouter>
@@ -11,11 +12,15 @@ const App: React.FC = (props) => {
 					pages={[
 						{ name: "home", path: "/" },
 						{ name: "about", path: "/about" },
+						{ name: "contact", path: "/contact" },
 					]}
 				/>
 				<Switch>
 					<Route path="/about">
 						<About />
+					</Route>
+					<Route path="/contact">
+						<Contact />
 					</Route>
 					<Route path="/">
 						<Main />
