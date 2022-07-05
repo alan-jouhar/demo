68bf2e00750d9d797aef8c86c4a0fbf6b76090ec
alan-jouhar <alan.jouhar@gmail.com>
2022-07-05 19:49:09 +0200

    Make the app work in non-strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import './global.css'
+import "./global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import store from './app/store'
+import store from "./app/store";
 
-let app = document.getElementById('app') as HTMLElement
+let app = document.getElementById("app") as HTMLElement;
 ReactDOM.render(
-	<React.StrictMode>
-		{/* <Provider store={store}> */}
-			<App />
-		{/* </Provider> */}
-	</React.StrictMode>
-,app);
+	<App />,
+	app
+);
