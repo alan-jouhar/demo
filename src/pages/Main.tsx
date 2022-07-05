22f0689dea1f06d4439e91cca020314758ea5728
alan-jouhar <alan.jouhar@gmail.com>
2022-07-05 19:51:50 +0200

    Fetch assets from the API

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CoinType, CoinResponse } from "../types/types";
+import CoinsWrapper from "../components/CoinsWarrper/CoinsWrapper";
+import Coin from "../components/Coin/Coin";
 const Main: React.FC = (props) => {
-	return <main>
-		<h1>Main Page</h1>
-	</main>;
+	const [assets, setAssets] = useState<CoinType[]>([]);
+	
+	useEffect(() => {
+		fetch('https://api.coincap.io/v2/assets')
+		.then(res => res.json())
+		.then(d => setAssets(d.data))
+	}, []);
+
+	useEffect(() => {
+		console.log(assets);
+	}, [assets]);
+
+	return (
+		<main className="mt-3">
+			<CoinsWrapper>
+				{assets.map((coin) => (
+					<Coin coin={coin} key={coin.id} />
+				))}
+			</CoinsWrapper>
+		</main>
+	);
 };
 
 export default Main;
