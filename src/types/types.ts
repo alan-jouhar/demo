705fdb536f3aa373e556cf7e13cd26c8388fa5a3
alan-jouhar <alan.jouhar@gmail.com>
2022-07-05 19:50:00 +0200

    Add global types directory to the project

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+// "id": "bitcoin",
+//     "rank": "1",
+//     "symbol": "BTC",
+//     "name": "Bitcoin",
+//     "supply": "17193925.0000000000000000",
+//     "maxSupply": "21000000.0000000000000000",
+//     "marketCapUsd": "119179791817.6740161068269075",
+//     "volumeUsd24Hr": "2928356777.6066665425687196",
+//     "priceUsd": "6931.5058555666618359",
+//     "changePercent24Hr": "-0.8101417214350335",
+//     "vwap24Hr": "7175.0663247679233209"
+export type CoinType = {
+    id:string,
+    rank:string,
+    symbol:string,
+    supply:string,
+    maxSupply:string,
+    marketCapUsd:string,
+    volumeUsd24Hr:string,
+    priceUsd:string,
+    changePercent24Hr:string,
+    vwap24Hr:string
+}
+
+export type CoinResponse = {
+    data:CoinType[]
+}
